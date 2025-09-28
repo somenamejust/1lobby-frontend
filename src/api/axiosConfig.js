ee4ab23455b96c17dc51472633579c5ac350591a
somenamejust <somenamejust@gmail.com>
2025-09-28 23:14:17 +0300

    Updated URLs and fixed deployment configuration

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log("Building with API URL:", process.env.REACT_APP_API_URL);
+
 // Создаем экземпляр axios с базовой конфигурацией
 const instance = axios.create({
   // Use the environment variable for production, fallback to localhost for development

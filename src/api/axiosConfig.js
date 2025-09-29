61d679dfd89d2ef5d2c06600c55ad60150add899
somenamejust <somenamejust@gmail.com>
2025-09-29 17:03:03 +0300

    Definitive Diagnostic

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Создаем экземпляр axios с базовой конфигурацией
 const instance = axios.create({
   // Use the environment variable for production, fallback to localhost for development
-  baseURL: window.API_URL || 'http://localhost:5000',
+  baseURL: 'http://164.92.250.91:5000',
   withCredentials: true
 });
 

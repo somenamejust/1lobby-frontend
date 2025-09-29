44003216f3de4b67486ec210bd32f5548132e9bb
somenamejust <somenamejust@gmail.com>
2025-09-29 15:14:23 +0300

    Updated Base URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-console.log("Building with API URL:", process.env.REACT_APP_API_URL);
-
 // Создаем экземпляр axios с базовой конфигурацией
 const instance = axios.create({
   // Use the environment variable for production, fallback to localhost for development
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://1lobby.xyz',
   withCredentials: true
 });
 

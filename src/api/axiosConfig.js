9ea45e3719ab43a3fc3b0708810978df36182cba
somenamejust <somenamejust@gmail.com>
2025-09-29 19:57:13 +0300

    Updated baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Создаем экземпляр axios с базовой конфигурацией
 const instance = axios.create({
   // Use the environment variable for production, fallback to localhost for development
-  baseURL: '/api',
+  baseURL: 'https://1lobby.xyz',
   withCredentials: true
 });
 
